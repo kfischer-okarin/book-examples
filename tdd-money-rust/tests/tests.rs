e7e536c14abd562b07985b4ff391289bfb8ee501
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-24 00:43:53 +0900

    Chapter 2: Change tests for times to return a new object (not compiling)

--- tdd-money-rust/tests/tests.rs
+++ tdd-money-rust/tests/tests.rs
@@ -2,7 +2,9 @@ use tdd_money_rust::money;
 
 #[test]
 fn multiplication() {
-    let mut five = money::Dollar::new(5);
-    five.times(2);
-    assert_eq!(five.amount, 10);
+    let five = money::Dollar::new(5);
+    let product = five.times(2);
+    assert_eq!(product.amount, 10);
+    let product = five.times(3);
+    assert_eq!(product.amount, 15);
 }
