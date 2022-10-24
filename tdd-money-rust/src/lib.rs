d1f51d933d50d87d86fdc68631f758c361ccc9cc
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-25 00:39:56 +0900

    Chapter 9: Add currency attribute

--- tdd-money-rust/src/lib.rs
+++ tdd-money-rust/src/lib.rs
@@ -1,17 +1,23 @@
 pub mod money {
     #[derive(Debug, PartialEq)]
-    pub enum Money {
-        Dollar { amount: i32 },
-        Franc { amount: i32 },
+    pub enum Money<'a> {
+        Dollar { amount: i32, currency: &'a str },
+        Franc { amount: i32, currency: &'a str },
     }
 
-    impl Money {
-        pub fn dollar(amount: i32) -> Money {
-            Money::Dollar { amount }
+    impl Money<'_> {
+        pub fn dollar(amount: i32) -> Money<'static> {
+            Money::Dollar {
+                amount,
+                currency: "USD",
+            }
         }
 
-        pub fn franc(amount: i32) -> Money {
-            Money::Franc { amount }
+        pub fn franc(amount: i32) -> Money<'static> {
+            Money::Franc {
+                amount,
+                currency: "CHF",
+            }
         }
 
         pub fn currency(&self) -> &str {
@@ -23,8 +29,8 @@ pub mod money {
 
         pub fn times(&self, multiplier: i32) -> Money {
             match self {
-                Money::Dollar { amount } => Money::dollar(amount * multiplier),
-                Money::Franc { amount } => Money::franc(amount * multiplier),
+                Money::Dollar { amount, .. } => Money::dollar(amount * multiplier),
+                Money::Franc { amount, .. } => Money::franc(amount * multiplier),
             }
         }
     }
