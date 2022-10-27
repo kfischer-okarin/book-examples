9f4fe9d74414c63293c5f697ae966f629c626269
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-27 14:35:19 +0900

    Chapter 18: Make test pass

--- tdd-xunit-ruby/tests.rb
+++ tdd-xunit-ruby/tests.rb
@@ -1,3 +1,5 @@
+require_relative 'xunit'
+
 test = WasRun.new('test_method')
 puts test.was_run?
 test.test_method
---
+++ tdd-xunit-ruby/xunit.rb
@@ -0,0 +1,14 @@
+class WasRun
+  def initialize(name)
+    @name = name
+    @was_run = false
+  end
+
+  def was_run?
+    @was_run
+  end
+
+  def test_method
+    @was_run = true
+  end
+end
