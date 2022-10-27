99b4b1afdd02b31ae2d193c551d09dfde3eb6535
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-27 14:41:20 +0900

    Move WasRun to tests file

--- tdd-xunit-ruby/tests.rb
+++ tdd-xunit-ruby/tests.rb
@@ -1,5 +1,20 @@
 require_relative 'xunit'
 
+class WasRun < TestCase
+  def initialize(name)
+    super
+    @was_run = false
+  end
+
+  def was_run?
+    @was_run
+  end
+
+  def test_method
+    @was_run = true
+  end
+end
+
 test = WasRun.new('test_method')
 puts test.was_run?
 test.run
--- tdd-xunit-ruby/xunit.rb
+++ tdd-xunit-ruby/xunit.rb
@@ -7,18 +7,3 @@ class TestCase
     send(@name)
   end
 end
-
-class WasRun < TestCase
-  def initialize(name)
-    super
-    @was_run = false
-  end
-
-  def was_run?
-    @was_run
-  end
-
-  def test_method
-    @was_run = true
-  end
-end
