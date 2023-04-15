1cb2cd2a375967fd57f82ec77a0fbc3f54810ed6
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-04-16 02:20:53 +0900

    Implement quick sort

--- modern-c/03-everything-is-about-control/challenge-1-sequential-sorting.c
+++ modern-c/03-everything-is-about-control/challenge-1-sequential-sorting.c
@@ -44,6 +44,77 @@ void merge_sort(size_t size, double input[size], double output[size]) {
   }
 }
 
+void quick_sort(size_t size, double input[size], double output[size]) {
+  if (size == 1) {
+    output[0] = input[0];
+    return;
+  }
+
+  size_t middle_index = (size_t) (size / 2.0);
+  double pivot = input[middle_index];
+  size_t size_left = 0;
+  for (int i = 0; i < size; ++i) {
+    if (i == middle_index) {
+      continue;
+    }
+
+    if (input[i] <= pivot) {
+      ++size_left;
+    }e
+  }
+  size_t size_right = size - size_left - 1;
+
+  double left[size_left];
+  double right[size_right];
+  for (int i = 0, left_index = 0, right_index = 0; i < size; ++i) {
+    if (i == middle_index) {
+      continue;
+    }
+
+    if (input[i] <= pivot) {
+      left[left_index] = input[i];
+      ++left_index;
+    } else {
+      right[right_index] = input[i];
+      ++right_index;
+    }
+  }
+
+  double sorted_left[size_left];
+  if (size_left > 0) {
+    quick_sort(size_left, left, sorted_left);
+  }
+  double sorted_right[size_right];
+  if (size_right > 0) {
+    quick_sort(size_right, right, sorted_right);
+  }
+
+  int output_index = 0;
+  for (int i = 0; i < size_left; ++i) {
+    output[output_index] = sorted_left[i];
+    ++output_index;
+  }
+  output[output_index] = pivot;
+  ++output_index;
+  for (int i = 0; i < size_right; ++i) {
+    output[output_index] = sorted_right[i];
+    ++output_index;
+  }
+}
+
+int assert_arrays_equal(size_t size, double array1[size], double array2[size]) {
+  for (int i = 0; i < size; ++i) {
+    if (array1[i] != array2[i]) {
+      printf("Array:\n");
+      print_array(size, array1);
+      printf("was not equal to:\n");
+      print_array(size, array2);
+      return EXIT_FAILURE;
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
 int main() {
   double array1[5] = {
     5.0,
@@ -55,7 +126,7 @@ int main() {
 
   double sorted_array1[5];
 
-  merge_sort(5, array1, sorted_array1);
+  quick_sort(5, array1, sorted_array1);
 
   double expected_sorted_array1[5] = {
     -3.4,
@@ -65,16 +136,12 @@ int main() {
     13.0,
   };
 
-  for (int i = 0; i < 5; ++i) {
-    if (sorted_array1[i] != expected_sorted_array1[i]) {
-      printf("Array was not sorted!\n");
-      print_array(5, sorted_array1);
-      return EXIT_FAILURE;
-    }
+  if (assert_arrays_equal(5, sorted_array1, expected_sorted_array1) == EXIT_SUCCESS) {
+    printf("Array sorted successfully with merge sort!\n");
+    print_array(5, sorted_array1);
+  } else {
+    return EXIT_FAILURE;
   }
-  printf("Array:\n");
-  print_array(5, array1);
-  printf("was sorted successfully:\n");
-  print_array(5, sorted_array1);
+
   return EXIT_SUCCESS;
 }
